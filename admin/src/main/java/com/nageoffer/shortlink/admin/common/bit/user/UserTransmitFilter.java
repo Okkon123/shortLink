563ec7c5a6a48a947c36b7bcc05fa22e72ecab23
Zuo Jie <118409022+Okkon123@users.noreply.github.com>
2024-03-16 21:48:52 +0800

    future: 增加项目打包插件

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/bit/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/bit/user/UserTransmitFilter.java
@@ -1,6 +1,7 @@
 package com.nageoffer.shortlink.admin.common.bit.user;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 用户信息传输过滤器
@@ -15,17 +17,24 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/has-username",
+            "/api/short-link/admin/v1/user/login"
+    );
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
-        if (!requestURI.equals("api/short-link/v1/user/login")) {
-            String userName = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(RedisCacheConstant.USER_LOGIN_KEY + userName, token);
-            if (userInfoJsonStr != null) {
-                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-                UserContext.setUser(userInfoDTO);
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(requestURI.equals("/api/short-link/admin/v1/user") && method.equals("POST"))) {
+                String userName = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(RedisCacheConstant.USER_LOGIN_KEY + userName, token);
+                if (userInfoJsonStr != null) {
+                    UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                    UserContext.setUser(userInfoDTO);
+                }
             }
         }
         try {
--- admin/src/test/java/com/nageoffer/shotlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shotlink/admin/test/UserTableShardingTest.java
@@ -16,9 +16,28 @@ public class UserTableShardingTest {
             "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
             ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
 
+    public static String linkSQL = "CREATE TABLE `t_link_%d`  (\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `domain` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '完整短链接',\n" +
+            "  `origin_url` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '原始链接',\n" +
+            "  `click_num` int NULL DEFAULT NULL COMMENT '点击量',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `enable_status` tinyint(1) NULL DEFAULT NULL COMMENT '启用标识 0:启用 1:禁用',\n" +
+            "  `create_type` tinyint(1) NULL DEFAULT NULL COMMENT '创建类型 0:',\n" +
+            "  `valid_date_type` tinyint(1) NULL DEFAULT NULL COMMENT '有效期类型',\n" +
+            "  `valid_date` datetime NULL DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '描述',\n" +
+            "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',\n" +
+            "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识 0:启用 1:禁用',\n" +
+            "  PRIMARY KEY (`id`) USING BTREE,\n" +
+            "  UNIQUE INDEX `idx_unique_full_short-url`(`full_short_url` ASC) USING BTREE\n" +
+            ") ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;" ;
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
-            System.out.printf((SQL) + "%n", i);
+            System.out.printf((linkSQL) + "%n", i);
         }
     }
 }
