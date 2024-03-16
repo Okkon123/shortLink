9a72e97c56173a6c8bcfd4beebfda8095e314a4b
Zuo Jie <118409022+Okkon123@users.noreply.github.com>
2024-03-17 00:33:37 +0800

    future: 短链接后管调用中台相关接口

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,50 @@
+package com.nageoffer.shortlink.admin.remote.dto;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.nageoffer.shortlink.admin.common.convention.result.Result;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLInkPageRespDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短链接中台远程调用服务
+ */
+public interface ShortLinkRemoteService {
+
+
+    /**
+     * 创建短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 短链接创建响应
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBody = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBody, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 短链接分页查询
+     * @param requestParam 分页短链接请求
+     * @return
+     */
+    default Result<IPage<ShortLInkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page", requestMap);
+        return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+
+
+}
