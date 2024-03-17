7c3f36da798c5b785c97bf545232015ca73ce5c9
Zuo Jie <118409022+Okkon123@users.noreply.github.com>
2024-03-17 14:54:25 +0800

    future: 短链接分组分表处理

--- admin/src/test/java/com/nageoffer/shotlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shotlink/admin/test/UserTableShardingTest.java
@@ -16,7 +16,18 @@ public class UserTableShardingTest {
             "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
             ") ENGINE=InnoDB AUTO_INCREMENT=1715030926162935810 DEFAULT CHARSET=utf8mb4;";
 
-    public static String linkSQL = "DROP TABLE IF EXISTS `t_link_%d";
+    public static String linkSQL = "CREATE TABLE `t_group_%d`  (\n" +
+            "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int NULL DEFAULT NULL COMMENT '分组排序',\n" +
+            "  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) NULL DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "  PRIMARY KEY (`id`) USING BTREE,\n" +
+            "  UNIQUE INDEX `idx_unique_username_gid`(`gid` ASC, `username` ASC) USING BTREE\n" +
+            ") ENGINE = InnoDB AUTO_INCREMENT = 1768634304847380482 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((linkSQL) + "%n", i);
