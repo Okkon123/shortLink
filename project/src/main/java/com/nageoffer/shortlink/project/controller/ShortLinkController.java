11a1011b12c29a17b3fa106237324c4a5ca19153
Zuo Jie <118409022+Okkon123@users.noreply.github.com>
2024-03-18 01:01:13 +0800

    future: 解决短链接跳转原始链接缓存击穿功能

--- project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
@@ -25,6 +25,9 @@ import java.util.List;
 public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
+    /**
+     * 短链接跳转原始链接
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, ServletRequest request, ServletResponse response) {
         shortLinkService.restoreUrl(shortUri, request, response);
