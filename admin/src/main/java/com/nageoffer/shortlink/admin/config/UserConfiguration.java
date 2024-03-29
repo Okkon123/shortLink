593dccf1a90dfe95c2250e599dea8adabe9b5139
Zuo Jie <118409022+Okkon123@users.noreply.github.com>
2024-03-29 14:39:08 +0800

    future: 短链接后管通过风控保障系统稳定性

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
@@ -1,6 +1,8 @@
 package com.nageoffer.shortlink.admin.config;
 
+import com.nageoffer.shortlink.admin.common.bit.user.UserFlowRiskControlFilter;
 import com.nageoffer.shortlink.admin.common.bit.user.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +24,17 @@ public class UserConfiguration {
         registration.addUrlPatterns("/*");
         registration.setOrder(0);
         return registration;
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+            StringRedisTemplate stringRedisTemplate,
+            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+        FilterRegistrationBean<UserFlowRiskControlFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(10);
+        return registrationBean;
     }
 }
